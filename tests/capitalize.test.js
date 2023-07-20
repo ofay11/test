a809ed337b9d7afd39d68944a7dae642ec04ee60
ofay11 <itimurdautov@gmail.com>
2023-07-20 22:08:59 +0300

    assert.

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -17,4 +17,19 @@ assert.equal(capitalize('hello'), 'Hello');
 assert.equal(capitalize('123'), '123');
 assert.equal(capitalize(''), '');
 
+assert.strictEqual('hello', 'hello'); // ок
+// strictEqual(actual, expected) проверяет равенство по ссылке
+// То есть два разных объекта, имеющих одинаковое содержание,
+// рассматриваются, как не эквивалентные:
+assert.strictEqual({}, {});     // ошибка
+assert.deepStrictEqual({}, {}); // ок
+
+// deepStrictEqual(params) можно вызывать как deepEqual(params)
+assert.deepStrictEqual({ key: 'value' }, { key: 'value' });   // ок
+assert.deepEqual({ key: 'value' }, { key: 'value' });         // ок
+assert.deepEqual({ key: 'value' }, { key: 'another value' }); // ошибка
+
+assert.notStrictEqual('hello', 'world');         // ок
+assert.notDeepStrictEqual({ a: 1 }, { a: '1' }); // ок
+
 console.log('Все тесты пройдены!');
