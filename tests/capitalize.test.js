f486026af3d200c2ac02d526f8ffcdfd830f75c0
ofay11 <itimurdautov@gmail.com>
2023-07-20 22:06:17 +0300

    assert.equal

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,20 @@
+import { strict as assert } from 'node:assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('123') !== '123') {
+//   throw new Error('Функция работает неверно!');
+// }
+
+// if (capitalize(''), '') {
+//   throw new Error('Функция работает неверно!');
+// }
+
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize('123'), '123');
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
